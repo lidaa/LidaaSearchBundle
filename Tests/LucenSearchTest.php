e302029df98d33e75c76d6779dea343429384cd2
lidaa <aa_dil@hotmail.fr>
2011-06-22 16:41:32 +0100

    Test for LucenSearch

---
+++ Tests/LucenSearchTest.php
@@ -0,0 +1,21 @@
+<?php 
+
+namespace Lidaa\SearchBundle\Tests;
+
+use Lidaa\SearchBundle\LucenSearch;
+
+class LucenSearchTest extends \PHPUnit_Framework_TestCase
+{
+    public function testGetIndex()
+    {
+    	$lucen_search = new LucenSearch(null, "./index", null);
+        $this->assertInstanceOf('Zend\Search\Lucene\Index', $lucen_search->getIndex());
+    }
+    
+    public function testGetDocument()
+    {
+    	$lucen_search = new LucenSearch(null, "./index", null);
+        $this->assertInstanceOf('Zend\Search\Lucene\Document', $lucen_search->getDocument());
+    }
+    
+}
